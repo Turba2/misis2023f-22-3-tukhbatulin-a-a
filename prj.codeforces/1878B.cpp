8c8b6771d1c8c12c8cd7804cf450e226656d91db
Turba2 <113940675+Turba2@users.noreply.github.com>
2023-10-12 17:09:51 +0300

    Add files via upload

---
+++ prj.codeforces/1878B.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+int main()
+{
+    int a;
+    std::cin >> a;
+    while (a--)
+    {
+        int n; 
+        std::cin >> n;
+        for (int i = 1; i <= 2 * n - 1; i += 2) std:: cout << i << " ";
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
